f878cbd11538444d01f25cfae3c5466cee04542a
The Game <chris.game@robbiebobbins.com>
2018-01-24 16:23:46 +0000

    stratify data for starbust chart

--- app/components/transaction-uploader/component.js
+++ app/components/transaction-uploader/component.js
@@ -8,6 +8,47 @@ export default Component.extend({
 
   store: service(),
 
+  transactionsGroupHierarchy: computed('accounts', function() {
+    let accounts = this.get('store').peekAll('account');
+    if (!accounts.get('length')) {
+      return [];
+    }
+
+    let account = accounts.get('firstObject');
+    let root = { name: account.get('accountNumber'), parent: null };
+
+    let uniqDescriptions = this.get('store').peekAll('transaction').mapBy('payee').uniq().sort();
+    let parents = uniqDescriptions.map(description => {
+      return { name: description, parent: account.get('accountNumber'), amount: null };
+    });
+
+    let children = this.get('store').peekAll('transaction').map(transaction => {
+      return {
+        name: transaction.get('memo'),
+        parent:  transaction.get('payee'),
+        value: transaction.get('value'),
+        direction: transaction.get('direction')
+      }
+    });
+
+    let preStratData = [
+      root,
+      ...parents.sort((a, b) => {
+        return b.total - a.total;
+      }),
+      ...children.sort((a, b) => {
+        return b.value - a.value;
+      })
+    ];
+
+    if (children.length) {
+      return d3.stratify()
+        .id(function(d) { return d.name; })
+        .parentId(function(d) { return d.parent; })
+        (preStratData);
+    }
+  }),
+
   actions: {
     handleFileUpload(evt) {
       let file = evt.target.files[0];
@@ -20,7 +61,7 @@ export default Component.extend({
             if (data[0] !== 'Number') {
               if (![...createdAccounts.keys()].includes(data[2])) {
                 let accountRecord = this.get('store').createRecord('account', {
-                  account: data[2]
+                  account: data[2].trim()
                 });
 
                 createdAccounts.set(data[2], accountRecord);
@@ -29,9 +70,9 @@ export default Component.extend({
               this.get('store').createRecord('transaction', {
                 date: data[1],
                 account: createdAccounts.get(data[2]),
-                amount: data[3],
-                subcategory: data[4],
-                memo: data[5]
+                amount: data[3].trim(),
+                subcategory: data[4].trim(),
+                memo: data[5].trim()
               });
             }
           });
