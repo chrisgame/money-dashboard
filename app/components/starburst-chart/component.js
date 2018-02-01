7869b13d6a1e5bf131c80f608d8f0b608ada708c
The Game <chris.game@robbiebobbins.com>
2018-02-01 22:29:57 +0000

    rendering mangled vectors from data - data is good

--- app/components/starburst-chart/component.js
+++ app/components/starburst-chart/component.js
@@ -11,30 +11,34 @@ export default Component.extend({
     let width = this.element.clientWidth;
     let height = width;
     let radius = Math.min(width, height) / 2;
+    //let color = d3.schemeCategory20c();
     let root = this.get('data');
 
-    let svg = d3.select(this.element).append('svg')
-        .attr('width', width)
-        .attr('height', height)
-      .append('g')
-        .attr('transform', 'translate(' + width / 2 + ',' + height * .52 + ')');
+    if (root.height) {
+      let svg = d3.select(this.element).append('svg')
+          .attr('width', width)
+          .attr('height', height)
+        .append('g')
+          .attr('transform', 'translate(' + width / 2 + ',' + height * .52 + ')');
 
-    var partition = d3.partition(root)
-        .size([2 * Math.PI, radius * radius]);
+      var partition = d3.partition()
+          .size([2 * Math.PI, radius * radius]);
 
-    var arc = d3.arc()
-        .startAngle(function(d) { return d.x; })
-        .endAngle(function(d) { return d.x + d.dx; })
-        .innerRadius(function(d) { return Math.sqrt(d.y); })
-        .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+      var arc = d3.arc()
+          .startAngle(function(d) { debugger; return d.x0; })
+          .endAngle(function(d) { return d.x1; })
+          .innerRadius(function(d) { return Math.sqrt(d.y0); })
+          .outerRadius(function(d) { return Math.sqrt(d.y1); });
 
-    var path = svg.datum(root).selectAll('path')
-        .data(partition)
-        .enter().append('path')
-        .attr('display', function(d) { return d.depth ? null : 'none'; }) // hide inner ring
-        .attr('d', arc)
-        .style('stroke', '#fff')
-        .style('fill', function(d) { return color((d.children ? d : d.parent).name); })
-        .style('fill-rule', 'evenodd');
+      var path = svg.selectAll('path')
+          .data(partition(root).descendants())
+          .enter().append('path')
+          .attr('display', function(d) { return d.depth ? null : 'none'; }) // hide inner ring
+          .attr('d', arc)
+          .style('stroke', '#fff')
+          //.style('fill', function(d) { return color((d.children ? d : d.parent).name); })
+          .style('fill', 'red')
+          .style('fill-rule', 'evenodd');
+    }
   }
 });
