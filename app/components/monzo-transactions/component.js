46817dc14e1fcdbc97d09f450e46b042f7e08c3a
The Game <chris.game@robbiebobbins.com>
2020-10-25 00:14:06 +0100

    add line chart component

--- app/components/monzo-transactions/component.js
+++ app/components/monzo-transactions/component.js
@@ -8,6 +8,16 @@ export default Component.extend({
     console.table(this.attrs.transactions.value.map((transaction) => transaction.toJSON()));
   },
 
+  width: computed(function() {
+    return Math.min(window.innerWidth, window.innerHeight) - 20;
+  }),
+
+  lineChartHeight: computed('width', function() {
+    let width = this.get('width');
+
+    return width / 3;
+  }),
+
   colorScale: computed('accounts', function() {
     let uniqDescriptions = this.store.peekAll('transaction').filterBy('direction', 'outgoing').mapBy('category').uniq().sort();
     return d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, uniqDescriptions.length + 1));
