74ee70c45c924512c2ac2ae24196fa55022e4b18
The Game <chris.game@robbiebobbins.com>
2022-05-12 21:08:57 +0100

    fix hover over line issue
    Switch to class based modifier to allow initial state to be separated
    from updates.
    
    Don't tear down the svg group when the styling of the paths change.
    
    If everything is torn down it causes the element to be appended after
    the mouse over box when the colour and stroke of the hovered line is
    updated. This in turn causes the mouse over box to loose focus and the
    colour and stroke of the line to be reverted.

--- app/modifiers/paths.js
+++ app/modifiers/paths.js
@@ -1,33 +1,120 @@
-import { modifier } from 'ember-modifier';
+import Modifier from 'ember-modifier';
 import d3 from 'd3';
 import { guidFor } from '@ember/object/internals';
+import { registerDestructor } from '@ember/destroyable';
 
-export default modifier((element, [], { data, x, y, color, strokeWidth = 2 }) => {
-    const id = `paths-${guidFor(element)}`;
+function cleanup(instance) {
+	document.getElementById(instance.id)?.remove();
+}
 
-    document.getElementById(id)?.remove();
+export default class PathsModifier extends Modifier {
+	data = undefined;
+	x = undefined;
+	y = undefined;
+	color = undefined;
+	strokeWidth = undefined;
+	lastData = undefined;
+	lastX = undefined;
+	lastY = undefined;
+	lastColor = undefined;
+	lastStrokeWidth = undefined;
+	id = undefined;
+	svg = undefined;
+	path = undefined;
+	line = undefined;
 
-    const svg = d3.select(element);
+  constructor(owner, args) {
+		super(owner, args);
 
-    const line = d3.line()
+    this.id = `paths-${guidFor(this)}`;
+
+		registerDestructor(this, cleanup);
+	}
+
+	didReceiveArguments() {
+		let {data, x, y, color, strokeWidth = 2} = this.args.named;
+
+		this.data = data;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.strokeWidth = strokeWidth;
+
+    if (this.isOnlyStylingUpdate) {
+			this.updateExistingPathsStyles();
+    }
+		else if (this.isFullUpdate) {
+			this.drawNewPaths();
+		};
+
+		this.lastData = data;
+		this.lastX = x;
+		this.lastY = y;
+		this.lastColor = color;
+		this.lastStrokeWidth = strokeWidth;
+  };
+
+	drawNewPaths() {
+    document.getElementById(this.id)?.remove();
+
+    this.svg = d3.select(this.element);
+
+    this.line = d3.line()
       .defined(d => !isNaN(d))
-      .x((d,i) => x(data.dates[i]))
-      .y(d => y(d));
+      .x((d,i) => this.x(this.data.dates[i]))
+      .y(d => this.y(d));
 
-    const path = svg.append('g')
-        .attr('id', id)
+    this.path = this.svg.append('g')
+        .attr('id', this.id)
         .attr('fill', 'none')
         .attr('stroke-linejoin', 'round')
-        .attr('stroke-linecap', 'round')
-      .selectAll('path')
-      .data(data.series)
-      .join('path')
-        .attr('stroke-width', typeof strokeWidth === 'function' ? d => strokeWidth(d.name) : strokeWidth)
-        .attr('stroke', d => color(d.name))
-        .attr('d', d => line(d.values));
+        .attr('stroke-linecap', 'round');
 
-  return () =>  {
-    /* Cleanup when the modifier is torn down */
-    document.getElementById(id)?.remove();
-  };
-});
+		this.path
+      .selectAll('path')
+      .data(this.data.series)
+      .join('path')
+        .attr('stroke-width', typeof this.strokeWidth === 'function' ? d => this.strokeWidth(d.name) : this.strokeWidth)
+        .attr('stroke', d => this.color(d.name))
+        .attr('d', d => this.line(d.values));
+	};
+
+  updateExistingPathsStyles() {
+		this.path
+      .selectAll('path')
+      .data(this.data.series)
+      .join('path')
+        .attr('stroke-width', typeof this.strokeWidth === 'function' ? d => this.strokeWidth(d.name) : this.strokeWidth)
+        .attr('stroke', d => this.color(d.name))
+        .attr('d', d => this.line(d.values));
+	};
+
+	get isOnlyStylingUpdate() {
+		if (
+			this.lastData === undefined ||
+			this.lastX === undefined ||
+			this.lastY === undefined
+		) {
+			return false;
+		};
+
+		return (
+			this.lastData === this.data &&
+			this.lastX === this.x &&
+			this.lastY === this.y
+		) &&
+		this.lastColor !== this.color ||
+		this.lastStrokeWidth !== this.strokeWidth
+	}
+
+	get isFullUpdate() {
+		let isNoUpdate =
+			this.lastData === this.data &&
+			this.lastX === this.x &&
+			this.lastY === this.y &&
+			this.lastColor === this.color &&
+			this.lastStrokeWidth === this.strokeWidth;
+
+		return !isNoUpdate;
+	}
+};
